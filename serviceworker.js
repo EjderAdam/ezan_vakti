5593701582cb0e6562e6994f1389b9f2703fe0ea
EjderAdam <2bozuk1bedava@gmail.com>
2022-12-13 22:27:25 +0300

    service worker

---
+++ serviceworker.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'cool-cache';
+
+// Add whichever assets you want to precache here:
+const PRECACHE_ASSETS = [
+    '/'
+]
+
+// Listener for the install event - precaches our assets list on service worker install.
+self.addEventListener('install', event => {
+    event.waitUntil((async () => {
+        const cache = await caches.open(CACHE_NAME);
+        cache.addAll(PRECACHE_ASSETS);
+    })());
+});
